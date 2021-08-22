a13432fd279077680e204bbe89d87e5c8ec88fb6
MD ABDUL KADER <77242923+nofelmedia@users.noreply.github.com>
2021-08-23 02:08:10 +0600

    github_user

---
+++ script.js
@@ -0,0 +1,31 @@
+var form = document.getElementById("myForm");
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    var search = document.getElementById("search").value
+    
+    var originalName = search.split('').join('')
+
+    document.getElementById("result").innerHTML = ""
+
+    fetch("https://api.github.com/users/"+originalName)
+    .then(result => result.json())
+    .then(data => {
+        console.log(data);
+
+        document.getElementById("result").innerHTML =`
+        <a target="blank" href="https://www.github.com/${originalName}"><img src="${data.avatar_url}"/></a>
+        `
+        document.getElementById("name").innerHTML =`
+        <a target="blank" href="https://www.github.com/${originalName}">Name: ${data.login}</a>
+        `
+        document.getElementById("followers").innerHTML =`
+        <a target="blank" href="https://www.github.com/${originalName}">Followers: ${data.followers}</a>
+        `
+        
+    })
+
+
+})
